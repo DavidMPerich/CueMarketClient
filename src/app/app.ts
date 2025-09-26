dd4fe2ba1b200448df045006e29ee95e7ce826d8
DavidMPerich <davidmperich@gmail.com>
2025-09-26 10:22:40 -0700

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
+export class App implements OnInit {
+  private http = inject(HttpClient)
+  protected title = 'Cue Market';
+  protected cues = signal<any>([]);
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7132/api/Cues').subscribe({
+      next: response => this.cues.set(response),
+      error: err => console.error(err),
+      complete: () => console.log('done')
+    });
+  }
 }
