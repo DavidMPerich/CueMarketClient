c345e9585bf33228df97d206c05f15445420566e
DavidMPerich <davidmperich@gmail.com>
2025-09-30 09:51:26 -0700

    add typescript types

--- src/core/services/account-service.ts
+++ src/core/services/account-service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
 
   baseUrl = 'https://localhost:7132/api/';
 
