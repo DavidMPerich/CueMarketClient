3d560482433348f50bfe72ce269b3201cb8068b1
DavidMPerich <davidmperich@gmail.com>
2025-10-06 19:07:36 -0700

    complete routing functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,26 +8,6 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
+export class App {
   protected router = inject(Router);
-  private http = inject(HttpClient)
-  protected title = 'Cue Market';
-  protected cues = signal<any>([]);
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:7132/api/Cues').subscribe({
-      next: response => this.cues.set(response),
-      error: err => console.error(err),
-      complete: () => console.log('done')
-    });
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
 }
