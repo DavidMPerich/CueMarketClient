60257593c97ea7ab8556e86e8236bd1b8f11ece9
DavidMPerich <davidmperich@gmail.com>
2025-10-28 19:09:08 -0700

    flex tab area

--- src/features/cues/cue-detailed/cue-detailed.ts
+++ src/features/cues/cue-detailed/cue-detailed.ts
@@ -1,13 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CueService } from '../../../core/services/cue-service';
 import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe, CommonModule, CurrencyPipe } from '@angular/common';
-import { Observable } from 'rxjs';
+import { CurrencyPipe } from '@angular/common';
 import { Cue } from '../../../types/cue';
 
 @Component({
   selector: 'app-cue-detailed',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, CurrencyPipe, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, CurrencyPipe, RouterOutlet],
   templateUrl: './cue-detailed.html',
   styleUrl: './cue-detailed.css'
 })
--- src/features/cues/cue-overview/cue-overview.ts
+++ src/features/cues/cue-overview/cue-overview.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cue } from '../../../types/cue';
 
 @Component({
   selector: 'app-cue-overview',
@@ -6,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './cue-overview.html',
   styleUrl: './cue-overview.css'
 })
-export class CueOverview {
+export class CueOverview implements OnInit {
+  private route = inject(ActivatedRoute);
+  protected cue = signal<Cue | undefined>(undefined);
 
+  ngOnInit(): void {
+    this.route.parent?.data.subscribe(data =>{
+      this.cue.set(data['cue']);
+    });
+  }
 }
